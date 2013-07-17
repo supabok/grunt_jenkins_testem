e3a00d20cc3269ebad0568c3b7e9cb1e54fe4287
supabok <superbok@superbokbok.com>
2013-07-17 17:33:36 +0900

    update .sh

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -1,5 +1,6 @@
 export PATH=/usr/local/bin:/path/to/node:/path/to/node_bin:/path/to/phantomjs:/path/to/jscoverage:$PATH;
 
+npm uninstall grunt
 npm install -g grunt-cli
 npm install
 grunt jenkins --no-color
